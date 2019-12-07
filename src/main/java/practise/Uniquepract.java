f5693b8f9fd4b148ad462cecbdfb713298aa1ba0
kikivaz <kikivaz@kikivaz-predator>
2019-12-07 17:27:51 +0530

    practice

---
+++ src/main/java/practise/Uniquepract.java
@@ -0,0 +1,21 @@
+package practise;
+
+public class Uniquepract {
+
+	public static void main(String[] args) {
+	
+		String names = ("abcdab");
+		char[] namearr = names.toCharArray();
+		
+		
+		// System.out.println(names);
+		int v=1;
+		for (int i=0;i<names.length();i++);
+		names.
+		
+		//System.out.printl([]);
+		
+		
+	}
+
+}
